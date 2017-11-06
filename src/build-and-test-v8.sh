01194297cf689b2274ca70dd6b2f7a33b8b9e7ab
Junliang Yan <jyan@ca.ibm.com>
2017-11-06 17:02:26 -0500

    return exit code if fail

--- src/build-and-test-v8.sh
+++ src/build-and-test-v8.sh
@@ -10,16 +10,21 @@ MACHINE=$(uname -m)
 V8="v8"
 BUILD=/workdir/build 
 NPROC=$(nproc)
+FAILED=0
 
 build_and_test() {
   BUILD_DIR=$1
   rm -f /result/$arch-*
-  script -c "gn gen $BUILD_DIR && ninja -C $BUILD_DIR all && \
+  script -e -c "gn gen $BUILD_DIR && ninja -C $BUILD_DIR all && \
              tools/run-tests.py -j $NPROC --time --progress=dots --timeout=120 --no-presubmit \
                                 --json-test-results=/result/$arch-test-result.json \
                                 --junitout=/result/$arch-junit.xml \
                                 --outdir=$BUILD_DIR --exhaustive-variants" \
     /result/$arch-script.log
+  if [[ $? -ne 0 ]] ; then
+    echo "Build and Test for $arch failed."
+    FAILED=1
+  fi
 }
 
 if [[ $MACHINE == "s390x" ]] ; then
@@ -39,4 +44,8 @@ cd $V8
 for arch in ${BUILD_ARCHES[@]} ; do 
   build_and_test $BUILD/$arch.release
 done
+
+if [[ $FAILED -eq 1 ]] ; then
+  exit 1
+fi
 
